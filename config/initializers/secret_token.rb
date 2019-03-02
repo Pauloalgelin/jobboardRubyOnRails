1c84289a35557c5a8159d8d2b7f119d2adc5fd5e
root <root@posi.araujo.com>
2019-03-02 11:25:42 -0300

    First commit. Initial files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard::Application.config.secret_token = '3c4e6270c10a1cab30b8b8092a9ae19e01f0b5f7a175582a7eaaf526432eac976a28460652bece92daaac6496641b222d5df7e6dcdab469dbb6feb2bd64411bb'
