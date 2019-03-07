4c603b8f97ddb9092bd6bdde40109980e7c488a4
Pauloalgelin <pauloalgelin@gmail.com>
2019-03-07 15:55:29 -0300

    Update db schema by rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20190302153648) do
+ActiveRecord::Schema.define(:version => 20190307184908) do
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "premium"
   end
 
 end
