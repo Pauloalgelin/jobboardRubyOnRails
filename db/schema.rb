85c720e36bbb4be7a1cda09c9711105928739245
Pauloalgelin <pauloalgelin@gmail.com>
2019-03-10 23:30:09 -0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20190307184908) do
+ActiveRecord::Schema.define(:version => 20190311022454) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "job_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
