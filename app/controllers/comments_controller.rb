d592dda8dadbc568de1726ce5dd28139221650f5
Pauloalgelin <pauloalgelin@gmail.com>
2019-03-15 14:12:19 -0300

    Comments controller and create action.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+    def create
+        @job = Job.find(params[:job_id])
+        @comment = @job.comments.build(params[:comment])
+        @comment.save
+        redirect_to @job
+    end
+end
