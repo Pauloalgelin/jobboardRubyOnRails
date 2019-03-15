2d551ce18149304bcec6681cfe7749a20f633ac4
Pauloalgelin <pauloalgelin@gmail.com>
2019-03-15 14:17:49 -0300

    Flash messages.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@ class CommentsController < ApplicationController
     def create
         @job = Job.find(params[:job_id])
         @comment = @job.comments.build(params[:comment])
-        @comment.save
+        if @comment.save
+            flash[:notice] = "Comment was created with success!"
+        else
+            flash[:alert] = "Please fill in all fields to create a comment."
+        end
         redirect_to @job
     end
 end
