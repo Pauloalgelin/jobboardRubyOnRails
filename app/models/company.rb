2e13f05b11475a6b372e428d5666f3ddb55fe797
Pauloalgelin <pauloalgelin@gmail.com>
2019-03-16 14:32:53 -0300

    Encrypted password.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
+require "bcrypt"
+
 class Company < ActiveRecord::Base
-  attr_accessible :email, :encrypted_password, :name
+  attr_accessible :email, :password, :name
+
+  def password=(new_password)
+    @password = new_password
+    self.encrypted_password = BCrypt::Password.create(@password)
+  end
+
+  def password
+    @password
+  end
+
 end
